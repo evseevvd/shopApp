2582ad54f7dbfdd026536ba3e1ab80734e46e02d
evseevvd <v.evseev.d@gmail.com>
2016-12-24 21:57:22 +0700

    Конфигурация Spring

---
+++ src/main/java/com/shop/configure/AppConfiguration.java
@@ -0,0 +1,26 @@
+package com.shop.configure;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Владимир on 24.12.2016.
+ *
+ * Класс конфигурации приложения
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan({
+        "com.shop.repository",
+        "com.shop.service",
+        "com.shop.web",
+})
+public class AppConfiguration extends WebMvcConfigurerAdapter{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
