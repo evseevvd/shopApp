59276b8a0f2beac449388c02d946e4f0a152b52b
evseevvd <evseevvd@sib-soft.ru>
2016-12-28 16:29:53 +0700

    Исправил ошибку

--- src/main/java/com/shop/web/rest/ProductRest.java
+++ src/main/java/com/shop/web/rest/ProductRest.java
@@ -1,17 +1,15 @@
 package com.shop.web.rest;
 
+import com.shop.repository.api.dto.ProductDto;
 import com.shop.repository.api.dto.response.ProductResponse;
 import com.shop.repository.api.dto.search.ProductSearchCriteria;
 import com.shop.service.api.ProductFacadeService;
-import com.shop.repository.api.dto.ProductDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 /**
  * Created by Владимир on 25.12.2016.
@@ -34,8 +32,8 @@ public class ProductRest {
         return productFacadeService.create(productDto);
     }
 
-    @PostMapping("/create")
-    public ProductResponse create(ProductSearchCriteria criteria) {
+    @PostMapping("/search")
+    public ProductResponse search(ProductSearchCriteria criteria) {
         return productFacadeService.search(criteria);
     }
 }
